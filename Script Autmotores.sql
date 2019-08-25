b9bbfbe41fa900023945597a3cee3a5c70dc217d
Alexis Toledo <alexistoledo@MacBook-Air-de-Alexis.local>
2019-08-25 19:58:53 -0300

    algunas tablas auxiliares cargadas

--- Script Autmotores.sql
+++ Script Autmotores.sql
@@ -1,6 +1,19 @@
--- Creacion de DB
+-- Creación de DB
 create database Automotores
 go
---- Seleccion de la DB
+--- Selección de la DB
 use Automotores
 
+-- Comenzamos creando las Tablas Auxiliares
+create table marcas_vehiculos (
+	id int identity (1,1) NOT NULL,
+	nombre varchar(50)
+	constraint pk_id primary key (id), 
+)
+create table modelos_vehiculos (
+	id int identity (1,1) NOT NULL,
+	nombre varchar(50),
+	id_marca int,
+	constraint pk_id primary key (id),
+	constraint fk_marcas foreign key (id) references marcas_vehiculos(id)
+)
